efb2bb26b946e8179fcb6df4558348db716b3151
李继峰 <lijifeng@91sph.com>
2020-06-12 11:34:04 +0800

    Pay

--- Demo/AppDelegate.m
+++ Demo/AppDelegate.m
@@ -19,7 +19,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     
-    
+    //fsdafas
 //1.test
     // 2.dev
     [TBCityIconFont setFontName:@"iconfont"];
